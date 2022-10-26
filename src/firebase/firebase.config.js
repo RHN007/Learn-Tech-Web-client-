7853b467a72a6e47fd83f5a9da68e07b9001cece
RHN007 <rakibul.nayon@outlook.com>
2022-10-26 17:55:47 +0600

    firebase deploy

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2-3ZkrM-xLFVWyO4wBl5lMYrU16EcfN8",
+  authDomain: "tech-learn-7bb79.firebaseapp.com",
+  projectId: "tech-learn-7bb79",
+  storageBucket: "tech-learn-7bb79.appspot.com",
+  messagingSenderId: "736533214299",
+  appId: "1:736533214299:web:f68dc512044a5ee8dc7b7e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
