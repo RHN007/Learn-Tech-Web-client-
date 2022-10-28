a6399622a75bf232aff44335d7c06b97ccec9d62
RHN007 <rakibul.nayon@outlook.com>
2022-10-28 20:37:24 +0600

    adding course details

---
+++ src/Pages/Shared/Courses/Courses.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
+
+const Courses = () => {
+    const courses = useLoaderData()
+  const  {title, details, image_url, category_id} = courses
+    return (
+        <Card >
+        <Card.Img variant="top" src={image_url}/>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {details}
+          </Card.Text>
+
+          <Link to={`/category/${category_id}`}> <Button variant="primary">All Courses in category</Button></Link>
+         
+        </Card.Body>
+      </Card>
+    );
+};
+
+export default Courses;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -7,6 +7,7 @@ import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Register/Register';
 import Blogs from '../Pages/Shared/Blogs/Blogs';
+import Courses from '../Pages/Shared/Courses/Courses';
 import TermsAndCondition from '../Pages/TermsAndCondition/TermsAndCondition';
 import PrivateRoutes from './PrivateRoutes/PrivateRoutes';
 
@@ -22,9 +23,14 @@ export const routes =  createBrowserRouter([
             }, 
             {
                 path: '/category/:id', 
-                element: <PrivateRoutes><Categories></Categories></PrivateRoutes>, 
+                element: <Categories></Categories>, 
                 loader: ({params}) => fetch(`http://localhost:5001/category/${params.id}`)
             }, 
+            {
+                path: '/course/:id', 
+                element: <PrivateRoutes><Courses></Courses></PrivateRoutes>, 
+                loader: ({params}) => fetch(`http://localhost:5001/courses/${params.id}`)
+            },
             {
                 path: '/login', 
                 element: <Login></Login>
